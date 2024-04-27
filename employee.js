13c5c5ecbd9515323947a383bdf50980a1b88f52
grassella <negrete.graciela@yahoo.com>
2024-04-27 16:54:47 -0500

    phase 4 completed

--- employee.js
+++ employee.js
@@ -9,14 +9,21 @@ class Employee {
             manager.addEmployee(this)
         }
     }
+
+    calculateBonus(multiplier){
+
+        const bonus = (this.salary) * multiplier
+        return bonus
+
+    }
 }
 
 
+const raph = new Employee('Raphael', 90000, 'Ninja');
+const donny = new Employee('Donatello', 85000, 'Grasshopper');
 
-const leo = new Employee('Leonardo', 90000, 'Ninja');
-// console.log(leo)
-
-// const raph = new Employee('Raphael', 90000, 'Ninja', splinter);
+// console.log(raph.calculateBonus(0.25)); // => 22500
+// console.log(donny.calculateBonus(0.15)); // => 12750
 
 
 module.exports = Employee
--- manager.js
+++ manager.js
@@ -6,29 +6,55 @@ class Manager extends Employee {
         super(name, salary, title, manager)
         this.employees = employees
         
-
+        
     }
 
 
     addEmployee(employee) {
+
         this.employees.push(employee)
+
     }
+
+
+    _totalSubSalary() {
+        
+        let sum = 0;
+
+        for (let i = 0; i < this.employees.length; i++) {
+            let employee = this.employees[i]
+            if (employee instanceof Manager) {
+                sum += employee.salary
+                return sum + employee._totalSubSalary()
+            }
+            else {
+                sum += employee.salary
+            }
+        }
+        return sum
+    }
+
+    calculateBonus(multiplier){
+
+        const bonus = ((this.salary) + this._totalSubSalary()) * multiplier
+        return bonus
+        
+    }
+
+
+   
 }
 
+    
 
-const splinter = new Manager('Splinter', 100000, 'Sensai');
-console.log('Before: ', splinter);
-
-const leo = new Employee('Leonardo', 90000, 'Ninja', splinter);
-const mikey = new Employee('Michelangelo', 90000, 'Ninja', splinter);
-const donnie = new Employee('Donatello', 90000, 'Ninja', splinter);
-const raph = new Employee('Raphael', 90000, 'Ninja', splinter);
-
-splinter.addEmployee(leo);
-splinter.addEmployee(mikey);
-splinter.addEmployee(donnie);
-splinter.addEmployee(raph);
-
-console.log('After: ', splinter);
+    const splinter = new Manager('Splinter', 100000, 'Sensei');
+    const leo = new Manager('Leonardo', 90000, 'Ninja', splinter);
+    const raph = new Manager('Raphael', 90000, 'Ninja', leo);
+    const mikey = new Employee('Michelangelo', 85000, 'Grasshopper', raph);
+    const donnie = new Employee('Donatello', 85000, 'Grasshopper', raph);
+    
+    console.log(splinter.calculateBonus(0.05)); // => 22500
+    // console.log(leo.calculateBonus(0.05)); // => 17500
+    // console.log(raph.calculateBonus(0.05)); // => 13000
 
 module.exports = Manager
